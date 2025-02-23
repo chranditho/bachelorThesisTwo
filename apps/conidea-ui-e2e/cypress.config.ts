620a99b82ea78d3e59bf0aefe01258651ad3aac1
christophthonhauser <c.thonhauser97@gmail.com>
2025-02-23 17:17:03 +0100

    migrate to inferred

--- apps/conidea-ui-e2e/cypress.config.ts
+++ apps/conidea-ui-e2e/cypress.config.ts
@@ -2,5 +2,12 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname),
+  e2e: nxE2EPreset(__dirname, {
+    ciWebServerCommand: 'npx nx run conidea-ui:serve-static',
+    webServerCommands: {
+      default: 'npx nx run conidea-ui:serve:development',
+      production: 'npx nx run conidea-ui:serve:production',
+      ci: 'npx nx run conidea-ui:serve-static',
+    },
+  }),
 });
