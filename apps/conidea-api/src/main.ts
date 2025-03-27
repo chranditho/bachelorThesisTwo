b189448ba4e50d585b18f7c43f019b213067dfeb
christophthonhauser <c.thonhauser97@gmail.com>
2025-03-27 13:42:11 +0100

    add nestjs swagger

--- apps/conidea-api/src/main.ts
+++ apps/conidea-api/src/main.ts
@@ -7,11 +7,20 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('ConIdea API')
+    .setDescription('The API provides access to data and operations')
+    .setVersion('1.0')
+    .addTag(globalPrefix)
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, documentFactory);
   app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port);
