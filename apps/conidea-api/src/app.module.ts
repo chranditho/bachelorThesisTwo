c6acd3dc6311e1dde0be87a49fcdd1668a0483bd
christophthonhauser <c.thonhauser97@gmail.com>
2025-05-16 14:13:55 +0200

    dockerize api

--- apps/conidea-api/src/app.module.ts
+++ apps/conidea-api/src/app.module.ts
@@ -7,7 +7,9 @@ import { DraftsModule } from './drafts/drafts.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/conidea'),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/conidea',
+    ),
     IdeasModule,
     UsersModule,
     DraftsModule,
